78123ffa8fcabea19853b9606e480c226a7ab58a
ValarDK <68809796+ValarDK@users.noreply.github.com>
2021-03-11 01:30:44 +0530

    Add files via upload

---
+++ src/mongoDB/Products.js
@@ -0,0 +1,36 @@
+db.Products.insertMany([
+    { "id": "13860428", 
+        "name": "The Big Lebowski (Blu-ray) (Widescreen)",
+         "currentPrice":
+          { 
+              "value": 13.49, 
+              "currencyCode": "USD" 
+           }
+     },
+     { "id": "54456119", 
+        "name": "Small pixel pitch LED video wall screen",
+         "currentPrice":
+          { 
+              "value": 122.0, 
+              "currencyCode": "USD" 
+           }
+     },
+      { "id": "13264003", 
+        "name": "Crystal-LED Integrated display (Widescreen)",
+         "currentPrice":
+          { 
+              "value": 133.49, 
+              "currencyCode": "USD" 
+           }
+     },
+       { "id": "12954218", 
+        "name": "The Big Lebowski - LED",
+         "currentPrice":
+          { 
+              "value": 143.9, 
+              "currencyCode": "USD" 
+           }
+     }
+     
+     
+     ]);  
